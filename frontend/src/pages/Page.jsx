7b34492b8f72b15c58c0a85fdfc09d4e68382bc5
JoshuaAAX <81261464+JoshuaAAX@users.noreply.github.com>
2023-05-06 19:20:21 -0500

    Update Page.jsx

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -8,7 +8,7 @@ export default function Page() {
     return (
       <Stack direction={['column']}>
          <Center bg='teal' h='100px' color='white' gap="10px">
-          This is the Center
+          This is the Center se estan haciendo los loquitos con el proyecto
          </Center>
          <Center bg='teal' h='100px' color='white' gap="10px">
           This is the Center
