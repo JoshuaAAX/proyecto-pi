014a6aba8a3520b010448c049ad09f1a2062257e
JoshuaAAX <joshua.chicame@correounivalle.edu.co>
2023-05-06 20:06:40 -0500

    new navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,25 +4,35 @@ import { Box,
          IconButton,
          Text,
          Spacer,
-         Button} from "@chakra-ui/react"
+         Button,
+         Menu,
+         MenuButton,
+         MenuList,
+         MenuItem} from "@chakra-ui/react"
 
 import { HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Navbar() {
-    return (
-
-        <Box bg="blue.300" h="56px" p="10px"> 
-           <Container maxW="80em">
-              <Flex alignItems='center' gap='2'>
-                 <IconButton colorScheme='blue' icon={<HamburgerIcon/>} />
-                 <Text fontSize='20px' color='white' as="b">
-                   LOGO
-                 </Text>
-                 <Spacer />
-                 <Button colorScheme='blue'>Log in</Button>
-              </Flex>   
-           </Container>
-        </Box>
-     
-    )
-}
+   return (
+     <Box bg="blue.300" h="56px" p="10px"> 
+       <Container maxW="80em">
+         <Flex alignItems='center' 
+               justify='space-between' 
+               gap="10px">
+           <IconButton colorScheme='blue' icon={<HamburgerIcon />} display={{ base: 'block' }} />
+           <Text fontSize='20px' color='white' as="b">
+             LOGO
+           </Text>
+           
+           <Spacer/>
+           <Flex alignItems='center' gap='2' display={{ base: 'block', md: 'flex' }}>
+             <Button colorScheme='blue'>
+               Log in
+             </Button>
+           </Flex>
+           
+         </Flex>   
+       </Container>
+     </Box>
+   );
+ }
