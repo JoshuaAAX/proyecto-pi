30feb9a8b2a300ad6d7d810d054a5573151f9571
Damian Espinosa <111003089+AsherAST@users.noreply.github.com>
2023-05-06 19:32:04 -0500

    Prueba deploy

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -7,8 +7,8 @@ import { Box,
 export default function Page() {
     return (
       <Stack direction={['column']}>
-         <Center bg='teal' h='100px' color='white' gap="10px">
-          This is the Center se estan haciendo los loquitos con el proyecto
+         <Center bg='teal' h='100px' color='black' gap="10px">
+          Hola este soy yo viendo si funcionó el deploy
          </Center>
          <Center bg='teal' h='100px' color='white' gap="10px">
           This is the Center
