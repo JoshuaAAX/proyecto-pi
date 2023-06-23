cbe1a3cc464d73760cb9bd4affb09e326daf58c5
KevinAlejandro17 <kevinalejandroabadia@gmail.com>
2023-06-23 11:17:43 -0500

    Fix production routes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -25,70 +25,72 @@ const Home = () => {
         container
         sx={{
           mt: 5,
-          ml: -20,
-
           mb: { md: 10, xs: 0 },
-          width: "70%",
+          width: "100%",
           height: "80%",
         }}
       >
-        <Canvas
-          shadows
-          gl={{ alpha: true }}
-          camera={{ position: [0, 0, 30], fov: 100 }}
-          style={{ width: "100%", height: "100%" }}
+        <Grid item md={5} xs={12}>
+          <Canvas
+            shadows
+            gl={{ alpha: true }}
+            camera={{ position: [0, 0, 30], fov: 100 }}
+            style={{ width: "100%", height: "100%" }}
+          >
+            <ambientLight intensity={0.7} />
+            <OrbitControls
+              makeDefault
+              minDistance={28}
+              maxDistance={28}
+              minPolarAngle={1}
+              maxPolarAngle={1.5}
+              enablePan={false}
+            />
+            <Estatua />
+          </Canvas>
+        </Grid>
+        <Grid
+          item
+          md={7}
+          px={1}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
         >
-          <ambientLight intensity={0.7} />
-          <OrbitControls
-            makeDefault
-            minDistance={30}
-            maxDistance={35}
-            minPolarAngle={1}
-            maxPolarAngle={1.5}
-          />
-          <Estatua />
-        </Canvas>
-      </Grid>
-      <Grid
-        item
-        xs={100}
-        mr={30}
-        md={8}
-        px={1}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography
-          variant="h4"
-          sx={{
-            mt: 5,
-            mb: { md: 0, xs: 10 },
-            width: { xs: "100%", md: "120%" },
-          }}
-        >
-          ¡Donde aprender sobre la cultura egipcia es fácil y divertido!
-        </Typography>
-        <Stack
-          direction={{ xs: "row", md: "column" }}
-          spacing={2}
-          sx={{
-            mt: 5,
-            mb: { md: 0, xs: 10 },
-            width: { xs: "100%", md: "100%" },
-            "& .MuiButton-root": {
-              color: "brown",
-              outline: "1px solid #DBB489",
-              backgroundColor: "rgba(219, 180, 137, 0.1)",
-            },
-          }}
-        >
-          <Button onClick={() => navigate("/signup")}>Crear una cuenta</Button>
-          <Button onClick={() => navigate("/login")}>
-            Ya tengo una cuenta
-          </Button>
-        </Stack>
+          <Typography
+            sx={{
+              mt: 5,
+              mb: { md: 0, xs: 5 },
+              width: { xs: "100%", md: "120%" },
+              fontSize: { xs: 20, md: 30 },
+              textAlign: "center",
+            }}
+          >
+            ¡Donde aprender sobre la cultura egipcia es fácil y divertido!
+          </Typography>
+          <Stack
+            direction={{ xs: "row", md: "column" }}
+            spacing={2}
+            sx={{
+              mt: { xs: 0, md: 5 },
+              mb: { md: 0, xs: 10 },
+              width: { xs: "100%", md: "60%" },
+              "& .MuiButton-root": {
+                color: "brown",
+                outline: "1px solid #DBB489",
+                backgroundColor: "rgba(219, 180, 137, 0.1)",
+              },
+            }}
+          >
+            <Button onClick={() => navigate("/signup")}>
+              Crear una cuenta
+            </Button>
+            <Button onClick={() => navigate("/login")}>
+              Ya tengo una cuenta
+            </Button>
+          </Stack>
+        </Grid>
       </Grid>
     </Container>
   );
--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,16 +1,51 @@
-import { Box, Button, Container, Grid } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Container, Grid, Skeleton } from "@mui/material";
+import { useEffect, useState } from "react";
 
 import { motion } from "framer-motion";
 import defaultPicture from "../../assets/default.png";
-
+import { supabase } from "../../backend/client";
+import { toast } from "react-hot-toast";
 
 const Test = () => {
   const [picture, setPicture] = useState(null);
+  const [uploaded, setUploaded] = useState(false);
 
-  const handleFileChange = ({ target }) => {
+  const handleDownload = async () => {
+    const { data: downloadData, error: downloadError } = await supabase.storage
+      .from("profile_pictures")
+      .download(`public/kevin.png`);
+    downloadData && !picture && setPicture(URL.createObjectURL(downloadData));
+    if (downloadError) toast.error(downloadError.message);
+  };
+
+  useEffect(() => {
+    handleDownload();
+    setUploaded(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [uploaded]);
+
+  const handleFileChange = async ({ target }) => {
     const newFile = target.files[0];
-    setPicture(URL.createObjectURL(newFile));
+
+    const { error: uploadError } = await supabase.storage
+      .from("profile_pictures")
+      .upload(`public/kevin.png`, newFile, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    if (uploadError) {
+      toast.error(uploadError.message);
+      const { error: updateError } = await supabase.storage
+        .from("profile_pictures")
+        .update(`public/kevin.png`, newFile, {
+          cacheControl: "3600",
+          upsert: true,
+        });
+      updateError ? toast.error(updateError.message) : setUploaded(true);
+    } else {
+      setUploaded(true);
+    }
   };
 
   return (
@@ -25,21 +60,25 @@ const Test = () => {
           rowGap: 3,
         }}
       >
-        <Grid
-          sx={{
-            backgroundImage: picture
-              ? `url(${picture})`
-              : `url(${defaultPicture})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            width: 250,
-            height: 250,
-            border: "1px dashed teal",
-            borderRadius: "16px",
-            boxShadow: "2px 2px 5px teal",
-          }}
-        />
+        {uploaded ? (
+          <Skeleton />
+        ) : (
+          <Grid
+            sx={{
+              backgroundImage: picture
+                ? `url(${picture})`
+                : `url(${defaultPicture})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              width: 250,
+              height: 250,
+              border: "1px dashed teal",
+              borderRadius: "16px",
+              boxShadow: "2px 2px 5px teal",
+            }}
+          />
+        )}
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <Button
             variant="contained"
